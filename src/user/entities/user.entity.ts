02eaf646f3bbf21a119bb3ae01a4bda433ec6a05
boom <boomtanut@gmail.com>
2021-10-14 02:56:32 +0700

    wrong

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,34 @@
-export class User {}
+import { Product } from 'src/user/entities/user.entity';
+import {
+    Column,
+    CreateDateColumn,
+    DeleteDateColumn,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    PrimaryColumn,
+    UpdateDateColumn,
+ } from 'typeorm';
+
+@Entity()
+export class User {
+    @PrimaryColumn()
+    id: number;
+    @Column()
+    buyer_id : number;
+    seller_id : number;
+    product_id : number;
+    amounts : number;
+    paid_price : number;
+    isPaymentDone : boolean;
+
+    @ManyToMany(() => User, user => user.courses)
+    @JoinTable()
+    users: User[];
+
+    @UpdateDateColumn()
+    updated_at: string;
+
+    @CreateDateColumn()
+    created_at: string;
+}
