f025e0f769533031b080bccddf7ed44225df586f
Anik Romyanon <The32069@hotmail.com>
2021-10-14 15:39:43 +0700

    product: added many to one

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -1,1 +1,30 @@
-export class Product {}
+
+import { User } from 'src/user/entities/user.entity';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    PrimaryColumn,
+    UpdateDateColumn,
+    PrimaryGeneratedColumn,
+    ManyToOne
+} from 'typeorm';
+
+@Entity({name : 'product'})
+export class Product {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name : string;
+    photo : string;
+    unit_sold : number;
+    @ManyToOne(() => User , user => user.products)
+    seller : User;
+    @UpdateDateColumn()
+    updated_at: string;
+
+    @CreateDateColumn()
+    created_at: string;
+}
